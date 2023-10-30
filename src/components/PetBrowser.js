2880fcee12f055560aff3a3ef324d10ad2d552e3
Gabriel Wortmann <wortmanngabriel@gmail.com>
2023-10-30 13:58:30 -0500

    run through code w/ Antonio

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,7 +2,10 @@ import React from "react";
 
 import Pet from "./Pet";
 
-function PetBrowser() {
+function PetBrowser({ pets, onAdoptPet }) {
+  const petCards = pets.map((pet) => (
+    <Pet key={pet.id} pet={pet} onAdoptPet={onAdoptPet}/>
+  ))
   return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
 }
 
